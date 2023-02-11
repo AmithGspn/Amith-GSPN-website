c3136e027db1f72afd00f4f2ade48875941f9c0c
declan <declanintech@gmail.com>
2023-02-11 15:38:08 +0100

    commit

--- packages/js/main.js
+++ packages/js/main.js
@@ -210,3 +210,11 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a').forEach(link => {
+  if(link.href.includes(`${activePage}`)){
+    link.classList.add('active-link')
+  }
+})
